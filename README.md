a0cca4508488cdb34eb211d3187a1503a3948585
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2022-06-21 17:13:45 +0200

    Chatbot 1.0

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-## Getting Started
+Chatbot
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Project for IBM-CIC.
+The entire project is coded by Kasper Dahl Andersen.
+Project entirely written in Java; at the moment I do not have a lot of experience with full-stack development.
+The terminal functions as the interface where commands in the form of strings can be inputted.
 
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Run with the following commands:
+javac Chatbot.java
+Java Chatbot
--- src/Chatbot.java
+++ src/Chatbot.java
@@ -9,11 +9,13 @@ public class Chatbot {
     private ArrayList<String> unavailable = unavailable();
 
     // hardcoded responses
-    String greetings = "\nChatbot: Greetings! Welcome to Bewhaos A/S automatic chatbot. \nType the name of the tool you are looking for: (type 'help' for more information on how I can help you)\n";
-    String goodbye = "\nChatbot: Goodbye! Please return if there is anything else I can help you with. :) \n";
+    String greetings = "\nChatbot: Greetings! Welcome to Bewhaos A/S automatic chatbot. \nType the name of the tool you are looking for (type 'help' to see your options).\n";
+    String goodbye = "\nChatbot: Goodbye! I hope to see you at Bewhaos A/S soon. :) \n";
     String help = "\nChatbot: Type the name of the tool you are looking for. \nType 'stock' to see current available tools. \nType 'goodbye' to exit. \n";
     String stock = "\nChatbot: We have the following tools in stock: ";
 
+    // constructor receives input. In switch statement the hardcoded strings are
+    // handled, otherwise it is passed on to handleInput()-method
     public Chatbot() {
         Scanner sc = new Scanner(System.in);
         System.out.println(greetings);
@@ -29,7 +31,7 @@ public class Chatbot {
                         System.out.println(help);
                         break;
                     case "stock":
-                        System.out.println(stock + productDB.keySet() + "\n");
+                        System.out.println(stock + "\n" + productDB.keySet() + "\n");
                         break;
                     default:
                         System.out.println(handleInput(input));
@@ -41,23 +43,23 @@ public class Chatbot {
         }
     }
 
-    // handles input besides exit, stock and help commands - decides what happens
-    // with the input afterward
+    // handles input besides hardcoded commands - decides what happens with the
+    // input afterward
     public String handleInput(String s) {
         String answer = "";
         if (!s.matches("^[a-zA-Z]*$")) {
-            answer = "Chatbot: Please only input single words in Latin alphabet (no whitespace, numbers, punctuations, etc.)";
+            answer = "Please only input single words in Latin alphabet (no whitespace, numbers, punctuations, etc.)";
         } else if (unavailable.contains(s)) {
-            answer = String.format("Chatbot: Sadly, we do not have any %s in stock.\nA new shipment is on its way.", s);
+            answer = String.format("Sadly, we do not have any %s in stock.\nA new shipment is on its way.", s);
         } else if (productDB.containsKey(s)) {
             answer = productDB.get(s);
         } else {
             answer = String.format(
-                    "Chatbot: We do not have any %s in our inventory.\nI will let our customer service team know you are looking for it.",
+                    "We do not have any %s in our inventory.\nI will let our customer service team know you are looking for it.",
                     s);
             notification(s);
         }
-        return "\n" + answer + "\n";
+        return "\nChatbot: " + answer + "\n";
     }
 
     public HashMap<String, String> fillProductDB() {
